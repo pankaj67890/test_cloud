72b3269db7cc15dcb2d3a2793303e2eb3ecce802
pankaj67890 <46005435+pankaj67890@users.noreply.github.com>
2019-12-21 23:54:36 +0530

    Add files via upload

---
+++ hello_gcs.py
@@ -0,0 +1,14 @@
+import logging
+
+def hello_gcs(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+    full_path=event['name']
+    pathString = full_path.split('/')
+    fileName = pathString[-1]
+    logging.info('New file-' +fileName+' has been uploaded to input bucket')
